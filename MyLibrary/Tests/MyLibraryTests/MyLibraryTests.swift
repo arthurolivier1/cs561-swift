87b8b7fa90183525c236ca37a2e8013c1dfd8f22
Arthur OLIVIER <arthurolivier69@gmail.com>
2022-10-20 13:07:33 -0700

    commit_final

--- MyLibrary/Tests/MyLibraryTests/MyLibraryTests.swift
+++ MyLibrary/Tests/MyLibraryTests/MyLibraryTests.swift
@@ -1,5 +1,5 @@
 import XCTest
-import MyLibrary
+@testable import MyLibrary
 
 final class MyLibraryTests: XCTestCase {
     func testIsLuckyBecauseWeAlreadyHaveLuckyNumber() async {
@@ -68,5 +68,45 @@ final class MyLibraryTests: XCTestCase {
         // Then
         XCTAssertNil(isLuckyNumber)
     }
+    
+    func testWeather() {
+        
+        let jsonString = """
+{
+  "main": {
+    "temp": 298.48,
+    "feels_like": 298.74,
+    "temp_min": 297.56,
+    "temp_max": 300.05,
+    "pressure": 1015,
+    "humidity": 64,
+    "sea_level": 1015,
+    "grnd_level": 933
+  }
+}
+"""
+        let jsonData = Data(jsonString.utf8)
+        let jsonDecoder = JSONDecoder()
+        let weather = try! jsonDecoder.decode(Weather.self, from: jsonData)
+        
+        if weather.main.temp==298.48 {
+            //print("the temperature was well recovered")
+        }
 
+    }
+    
+    func testWeatherServiceImpl_mock_server() async {
+        // Given
+        let library = MyLibrary()
+        
+        // When
+        let lucky = await library.isLucky(2)
+        
+        // Then
+        XCTAssertNotNil(lucky)
+        XCTAssert(lucky == true)
+        
+    }
+    
+    
 }
