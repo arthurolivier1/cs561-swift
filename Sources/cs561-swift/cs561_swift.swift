5809b20913414541358165985d2632864a3e72ba
Arthur OLIVIER <arthurolivier69@gmail.com>
2022-10-21 16:32:18 -0700

    github-actions

---
+++ Sources/cs561-swift/cs561_swift.swift
@@ -0,0 +1,6 @@
+public struct cs561_swift {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/cs561-swiftTests/cs561_swiftTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import cs561_swift
+
+final class cs561_swiftTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(cs561_swift().text, "Hello, World!")
+    }
+}
